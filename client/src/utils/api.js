89ecc34af3817d07f16279aa10afdc4864ae2d44
Barath-K-R <barath223637@gmail.com>
2023-08-31 22:13:58 +0530

    updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://hotelbookingapp-omega.vercel.app/api",
+  baseURL: "https://hotelbookingapp-client.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
